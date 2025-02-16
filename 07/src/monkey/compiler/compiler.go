e71fec59312d8676bea67fa50ba630920b94ac4d
djgns0174 <djgns0174@naver.com>
2025-02-16 14:53:25 +0900

    07. 함수 지역바인딩 - 지역바인딩 컴파일하기 - 심벌 테이블 확장하기 - 스코프가 있는 바인딩 컴파일하기

--- 07/src/monkey/compiler/compiler.go
+++ 07/src/monkey/compiler/compiler.go
@@ -112,7 +112,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 			return err
 		}
 		symbol := c.symbolTable.Define(node.Name.Value)
-		c.emit(code.OpSetGlobal, symbol.Index)
+		if symbol.Scope == GlobalScope {
+			c.emit(code.OpSetGlobal, symbol.Index)
+		} else {
+			c.emit(code.OpSetLocal, symbol.Index)
+		}
 
 	case *ast.PrefixExpression:
 		err := c.Compile(node.Right)
@@ -175,7 +179,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if !ok {
 			return fmt.Errorf("undefined variable %s", node.Value)
 		}
-		c.emit(code.OpGetGlobal, symbol.Index)
+		if symbol.Scope == GlobalScope {
+			c.emit(code.OpGetGlobal, symbol.Index)
+		} else {
+			c.emit(code.OpGetLocal, symbol.Index)
+		}
 
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
@@ -355,6 +363,7 @@ func (c *Compiler) enterScope() {
 		previousInstruction: EmittedInstruction{},
 	}
 
+	c.symbolTable = NewEnclosedSymbolTable(c.symbolTable)
 	c.scopes = append(c.scopes, scope)
 	c.scopeIndex++
 }
@@ -362,6 +371,7 @@ func (c *Compiler) enterScope() {
 func (c *Compiler) leaveScope() code.Instructions {
 	instructions := c.currentInstructions()
 
+	c.symbolTable = c.symbolTable.Outer
 	c.scopes = c.scopes[:len(c.scopes)-1]
 	c.scopeIndex--
 
